0afd37e1fe26901f89040df94217b9cb72d3bdca
n-gregory-a <ovonely@gmail.com>
2019-10-17 21:02:39 +0700

    Autowiring - setter injection: add setter method in TennisCoach class for dependency injection

--- src/gn/springdemo/AnnotationDemoApp.java
+++ src/gn/springdemo/AnnotationDemoApp.java
@@ -12,11 +12,11 @@ public class AnnotationDemoApp {
         // get the bean from spring container
         Coach tennisCoach = context.getBean("tennisCoach", Coach.class);
 
-        Coach skiCoach = context.getBean("skiCoach", Coach.class);
+        //Coach skiCoach = context.getBean("skiCoach", Coach.class);
 
         // call a method on the bean
         System.out.println("Tennis coach says: " + tennisCoach.getDailyWorkout());
-        System.out.println("And ski coach says: " + skiCoach.getDailyWorkout());
+        //System.out.println("And ski coach says: " + skiCoach.getDailyWorkout());
 
         // call method to get daily fortune
         System.out.println(tennisCoach.getDailyFortune());
