10b3af61878f12bb82d552ee728d00335d4a2941
n-gregory-a <ovonely@gmail.com>
2019-10-19 20:56:33 +0700

    Studying about life cycle annotation: @PostConstruct and @PreDestroy

--- src/gn/springdemo/TennisCoach.java
+++ src/gn/springdemo/TennisCoach.java
@@ -1,14 +1,16 @@
 package gn.springdemo;
 
+import org.omg.CORBA.PUBLIC_MEMBER;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
 import java.io.FileNotFoundException;
 
 @Component
-@Scope("prototype")
 public class TennisCoach implements Coach {
 
     @Qualifier("fromFileFortuneService")
@@ -16,9 +18,22 @@ public class TennisCoach implements Coach {
     private FortuneService fortuneService;
 
     //define default constructor
-
     public TennisCoach() {
         System.out.println(">> TennisCoach: inside default constructor");
+    }
+
+    // define my init method
+    @PostConstruct
+    public void doMyStartUpStuff() {
+
+        System.out.println(">> TennisCoach: doMyStartUpStuff()");
+
+    }
+
+    // define my destroy method
+    @PreDestroy
+    public void doMyCleanUpStuff() {
+        System.out.println(">> TennisCoach: doMyCleanUpStuff()");
     }
 
 //    @Autowired
